dbc240da339275ce78113365915ce521873b901d
Alan Tai <alan.tai@zuhlke.com>
2019-05-02 13:54:48 +0800

    Initial commit

---
+++ src/main/java/com/zuhlke/interview/CodingChallenge.java
@@ -0,0 +1,4 @@
+package com.zuhlke.interview;
+
+public class CodingChallenge {
+}
---
+++ src/test/java/com/zuhlke/interview/CodingChallengeTest.java
@@ -0,0 +1,11 @@
+package com.zuhlke.interview;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CodingChallengeTest {
+    @Test
+    public void test() {
+        Assert.assertEquals("Input string is 1", 1, Integer.parseInt("1"));
+    }
+}
