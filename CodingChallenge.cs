eb9905dc5eb3567c020265206193b77116dac3a7
Alan Tai <alan.tai@zuhlke.com>
2019-06-22 12:59:49 +0800

    Initial commit

---
+++ CodingChallenge.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace interview
+{
+    public class CodingChallenge
+    {
+        public static int Convert(string romanNumerals)
+        {
+            return 1;
+        }
+
+        [Fact]
+        public void TestConvertI()
+        {
+            Assert.Equal(1, CodingChallenge.Convert("I"));
+        }
+    }
+}
